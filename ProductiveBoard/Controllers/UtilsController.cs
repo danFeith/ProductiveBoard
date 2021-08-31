3eb426117fcab672665b8edf2c966732f7e827de
nadav dayagi <nadav.dyg@gmail.com>
2021-08-31 16:40:41 +0300

    edit about page data and update utils

--- ProductiveBoard/Controllers/UtilsController.cs
+++ ProductiveBoard/Controllers/UtilsController.cs
@@ -20,10 +20,10 @@ namespace ProductiveBoard.Controllers
             LocationLists model = new LocationLists();
             var locations = new List<Locations>()
             {
-                new Locations(1, "Ilan Malisov","Herzliya, israel", 32.163510, 34.842670),
-                new Locations(2, "Michael Tsiriulinikov","Bat Yam, israel", 32.011850, 34.745310),
-                new Locations(3, "Ofek Reuveni","Rishon Lezion, israel", 31.963760, 34.815720),
-                new Locations(3, "Colman Collage of management","Rishon Lezion, israel", 31.986460, 34.806020)
+                new Locations(1, "Ilan Malisov - ilan07m@gmail.com","Herzliya, israel", 32.163510, 34.842670),
+                new Locations(2, "Michael Tsiriulinikov - misha1235000@gmail.com","Bat Yam, israel", 32.011850, 34.745310),
+                new Locations(3, "Ofek Reuveni - ofekrv@gmail.com","Rishon Lezion, israel", 31.963760, 34.815720),
+                new Locations(3, "Colman Collage of management - 03-963-4000","Rishon Lezion, israel", 31.986460, 34.806020)
             };
             model.LocationList = locations;
             return View(model);
