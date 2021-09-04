a323a25e84588c0f644987ab89420426d891a490
ido philip <idophilip1212@gmail.com>
2021-09-04 23:02:42 +0300

    bug fix

--- ProductiveBoard/Controllers/UtilsController.cs
+++ ProductiveBoard/Controllers/UtilsController.cs
@@ -18,9 +18,9 @@ namespace ProductiveBoard.Controllers
             var locations = new List<Locations>()
             {
                 new Locations(1, "Nadav Dayagi - NadavDyg@gmail.com","Herzliya, israel", 32.163510, 34.842670),
-                new Locations(2, "Ido Philip - IdoPhilip@gmail.com","Bat Yam, israel", 32.011850, 34.745310),
+                new Locations(2, "Ido Philip - IdoPhilip@gmail.com","Meitar, israel", 31.316412, 34.932715),
                 new Locations(3, "Dan Feithlicher - DanFeith@gmail.com","Rishon Lezion, israel", 31.963760, 34.815720),
-                new Locations(3, "Itamar Higani - iHigani@gmail.com","Rishon Lezion, israel", 31.965760, 34.815320),
+                new Locations(3, "Itamar Higani - iHigani@gmail.com","Tel Aviv, israel", 32.068020, 34.767550),
                 new Locations(3, "Colman Collage of management - 03-963-4000","Rishon Lezion, israel", 31.986460, 34.806020)
             };
             model.LocationList = locations;
